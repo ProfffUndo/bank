e58358074ce60d99fbc89bc8b75b812c24bd3e91
ProfffUndo <smyslov4@gmail.com>
2023-11-03 00:43:36 +0300

    bank#2 Создать необходимые сущности и таблицы в БД

---
+++ src/main/java/com/profffundo/bank/entity/Transaction.java
@@ -0,0 +1,29 @@
+package com.profffundo.bank.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transaction")
+@Data
+public class Transaction {
+    @Id
+    @JoinColumn(name = "tran_id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private Account senderAccount;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private Account receiverAccount;
+
+    private Double value;
+
+    @JoinColumn(name = "tran_time")
+    private LocalDateTime time;
+
+}
